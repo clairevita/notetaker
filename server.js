90c91e4b5cd4457931ccadcf3b2b5f5a64cc03f3
Claire Vita <clairej.vita@gmail.com>
2020-11-16 13:07:54 -0500

    Finalized comments

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+//Here we are telling the host to use the Express framework.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//Here we are directing the app to reference the public folder in order to format the page appropriately.
 app.use(express.static('public'))
 
+//This is establishing the proper routes for the app to reference. 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-
+//This initiates the server.
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 }); 
